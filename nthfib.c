011964f88295c71c146c602403580bcbf57e2ca6
dargaCode <darga.code@gmail.com>
2015-10-05 17:59:02 -0400

    improved text output

--- nthfib.c
+++ nthfib.c
@@ -41,7 +41,8 @@ int main(int argc, string argv[])
     // add one extra index to more easily count from 1
     long long memoized[n + 1];
     long long output = get_nth_fib(n, memoized);
-    printf("Fibonacci Number #%i = %lli \n", n, output);
+    printf("\nFinal result: Fibonacci Number #%i = %lli \n\n",
+        n, output);
 
     // success
     return 0;
@@ -67,6 +68,9 @@ void memoize_nth(long long memoized[], int n, long long fib)
 {
     memoized[n] = fib;
     memoized_count++;
+    printf(COLOR_YELLOW);
+    printf("Memoizing Fib #%i = %lld\n", n, fib);
+    printf(COLOR_RESET);
 }
 
 bool is_nth_memoized(int n)
@@ -96,12 +100,14 @@ long long get_nth_fib(int n, long long memoized[])
 {
     long long result;
     recursion_count++;
-    printf("Recursive calls: %i\n", recursion_count);
-
+    printf(COLOR_CYAN);
+    printf("\n(Recursive calls: %i)\n", recursion_count);
+    printf(COLOR_RESET);
     // already memoized
     if (is_nth_memoized(n))
     {
         result = get_nth_memoized(memoized, n);
+        printf("Retrieved Fib #%i: %lld\n", n, result);
     }
     // needs to be memoized
     else
@@ -109,17 +115,25 @@ long long get_nth_fib(int n, long long memoized[])
         if (n == 1 || n == 2)
         {
             result = n - 1;
+            printf(COLOR_GREEN);
+            printf("Reached base case! Fib #%i: %lld\n",
+                n, result);
+            printf(COLOR_RESET);
         }
         // keep recursing
         else
         {
-            long long prev_a = get_nth_fib(n - 1, memoized);
-            long long prev_b = get_nth_fib(n - 2, memoized);
+            printf("Recurse Fib #[%i-2]...\n", n);
+            long long prev_a = get_nth_fib(n - 2, memoized);
+            printf("\nRecurse Fib #[%i-1]...\n", n);
+            long long prev_b = get_nth_fib(n - 1, memoized);
             result = prev_a + prev_b;
+            printf("\nCalculate Fib #%i: %lld + %lld = %lld\n",
+                n, prev_a, prev_b, result);
         }
         // memoize result
         memoize_nth(memoized, n, result);
+
     }
-    printf("Fib #%i: %lld \n", n, result);
     return result;
 }
