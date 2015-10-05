4cf90701f7f6bdc464d2323d5f77711a13aede42
dargaCode <darga.code@gmail.com>
2015-10-05 19:37:33 -0400

    pre-memoizing values 1 and 2 rather than waiting to reach them recursively

--- nthfib.c
+++ nthfib.c
@@ -40,6 +40,11 @@ int main(int argc, string argv[])
     int n = atoi(argv[1]);
     // add one extra index to more easily count from 1
     long long memoized[n + 1];
+    // memoize first two values
+    memoized[0] = -1;
+    memoized[1] = 0;
+    memoized[2] = 1;
+    memoized_count = 3;
     long long output = get_nth_fib(n, memoized);
     printf("\nFinal result: Fibonacci Number #%i = %lli \n\n",
         n, output);
@@ -76,7 +81,7 @@ void memoize_nth(long long memoized[], int n, long long fib)
 bool is_nth_memoized(int n)
 {
     // adding one since counting from 1, not 0
-    bool result = memoized_count >= n + 1;
+    bool result = memoized_count > n;
     if (result)
     {
         printf(COLOR_GREEN);
@@ -100,40 +105,33 @@ long long get_nth_fib(int n, long long memoized[])
 {
     long long result;
     recursion_count++;
+
     printf(COLOR_CYAN);
     printf("\n(Recursive calls: %i)\n", recursion_count);
     printf(COLOR_RESET);
+
     // already memoized
     if (is_nth_memoized(n))
     {
         result = get_nth_memoized(memoized, n);
         printf("Retrieved Fib #%i: %lld\n", n, result);
     }
-    // needs to be memoized
+    // needs to be calculated
     else
     {
-        if (n == 1 || n == 2)
-        {
-            result = n - 1;
-            printf(COLOR_GREEN);
-            printf("Reached base case! Fib #%i: %lld\n",
-                n, result);
-            printf(COLOR_RESET);
-        }
-        // keep recursing
-        else
-        {
-            printf("Recurse Fib #[%i-2]...\n", n);
-            long long prev_a = get_nth_fib(n - 2, memoized);
-            printf("\nRecurse Fib #[%i-1]...\n", n);
-            long long prev_b = get_nth_fib(n - 1, memoized);
-            result = prev_a + prev_b;
-            printf("\nCalculate Fib #%i: %lld + %lld = %lld\n",
-                n, prev_a, prev_b, result);
-        }
+        // calculate recursively
+        printf("\nRecurse Fib #[%i-1]...\n", n);
+        long long prev_a = get_nth_fib(n - 1, memoized);
+
+        printf("Recurse Fib #[%i-2]...\n", n);
+        long long prev_b = get_nth_fib(n - 2, memoized);
+        result = prev_a + prev_b;
+
+        printf("\nCalculate Fib #%i: %lld + %lld = %lld\n",
+            n, prev_a, prev_b, result);
+
         // memoize result
         memoize_nth(memoized, n, result);
-
     }
     return result;
 }
